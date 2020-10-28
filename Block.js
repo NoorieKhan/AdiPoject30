be3aeadbba57bf8dfaa3c3e8f49020549e89ad91
NoorieKhan <nooriezak@gmail.com>
2020-10-28 08:18:58 +0530

    changed

--- Block.js
+++ Block.js
@@ -1,10 +1,12 @@
 class Block{
-    constructor(x, y, width, height, color) {
+    constructor(x, y, width, height) {
+     
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            'restitution':0.4,
+            'friction': 0.0
+            
         }
+      
         this.visibility = 225;
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
@@ -12,11 +14,14 @@ class Block{
         World.add(world, this.body);
       }
       display(){
+       console.log(this.body.speed)
         if(this.body.speed<3){
-        var angle = this.body.angle;
+       
         push();
         translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
+        
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
         pop();
         }
         else{
